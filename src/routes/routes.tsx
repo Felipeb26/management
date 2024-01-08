dc7d1d3b395f4f34e8512fc08a9a086abd50b9aa
FelipeB26 <felipeb2silva@hotmail.com>
2024-01-08 03:00:59 -0300

    fixed basename

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,23 +4,30 @@ import ContatosComponent from "../screens/contatos";
 import EmployeesComponent from "../screens/employees";
 import EmployeeComponent from "../screens/employee";
 
-const Router = createBrowserRouter([
-	{
-		path: "/",
-		element: <App />,
-	},
-	{
-		path: "employee/:id",
-		element: <EmployeeComponent />,
-	},
-	{
-		path: "employees",
-		element: <EmployeesComponent />,
-	},
-	{
-		path: "contato",
-		element: <ContatosComponent />,
-	},
-]);
+const Router = createBrowserRouter(
+	[
+		{
+			path: "/",
+			element: <App />,
+		},
+		{
+			path: "",
+			element: <App />,
+		},
+		{
+			path: "employee/:id",
+			element: <EmployeeComponent />,
+		},
+		{
+			path: "employees",
+			element: <EmployeesComponent />,
+		},
+		{
+			path: "contato",
+			element: <ContatosComponent />,
+		},
+	],
+	{ basename: "/management" }
+);
 
 export default Router;
